b264e7d18c82ad35a6d78f3ed1a988ff3edb5239
Justin <justinJustin@nadjas-imac.mynetworksettings.com>
2023-03-28 16:22:11 -0400

    updates drinks.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express")
-
 const app = express()
-
 const port = 3000
+const drinks = require("./models/drinks.js")
 
 
 app.get ("/", (req, res) => {
@@ -11,13 +10,13 @@ app.get ("/", (req, res) => {
 
 
 
-// app.get("/gitpub/", (req, res) => {
-//         res.send(gitpub)
-//       })
+app.get("/drinks.js/", (req, res) => {
+        res.send(drinks)
+      })
 
-//       app.get("/gitpub/:indexOfGitpubArray", (req,res) => {
-//     console.log(req.params.indexOfGitpubArray)
-// })
+      app.get("/drinks.js/:indexOfDrinksArray", (req,res) => {
+    console.log(req.params.indexOfDrinksArray)
+})
 
 app.listen(3000, () => {
     console.log(`Welcome to the Gitpub App! ${port}`)
